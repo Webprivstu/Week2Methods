7b9a26118904a9dbf6fecece9ff6acd5908cd323
webprivstu <beggs-g4@ulster.ac.uk>
2025-09-29 15:31:46 +0100

    new method code to program

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Main();
+
+void Main()
+{
+    try
+    {
+        Console.WriteLine("PLease enter rectangle length");
+        double length = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Please enter rectangle width");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+       
+        double area = CalculateArea(length, width);
+
+        
+        Console.WriteLine($"The area of the rectangle is: {area}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+    }
+}
+
+
+static double CalculateArea(double length, double width)
+{
+   
+    double area = length * width;
+    return area; 
+}
